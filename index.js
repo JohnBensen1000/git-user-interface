0cf0ae372b4f80dc40422d3e9022fe68f4abf18e
John Bensen <jbensen@purdue.edu>
2020-07-15 22:51:43 -0400

    allows user to set path and create folder

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('fork-main').addEventListener('click', () => {
-    const { exec } = require("child_process");
+const { exec } = require("child_process");
 
-    exec("dir", (error, stdout, stderr) => {
-        if (error) {
-            document.getElementById("output").innerHTML = `error: ${error.message}`;
-            return;
-        }
-        if (stderr) {
-            document.getElementById("output").innerHTML = `stderr: ${stderr}`;
-            return;
-        }
-        document.getElementById("output").innerHTML = `stdout: ${stdout}`;
+function execute_command(command) {
+    /*  exec function used to run a command in the command prompt, returns an error 
+        if there is an error while running
+        Parameters:
+          command (str): the specific command that is executed
+    */
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+          document.getElementById("output").innerHTML = "An error has occured";
+          return;
+      }
+      if (stderr) {
+          document.getElementById("output").innerHTML = "A stderr has occurred";
+          return;
+      }
     });
+}
+
+var repoPath = "";  // string, the path to the repository
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('userDir').addEventListener('click', () => {
+        // user inputs path to repository, empties input after saving string
+        repoPath = document.getElementById("repo_path").value;
+        document.getElementById("repo_path").value = "";
+    });
+    document.getElementById('addFolder').addEventListener('click', () => {
+        // user inputs name of new folder, need to add path to repository to put the folder
+        // in the right place, calles execute_command to run command, empties input
+        execute_command("cd " + repoPath  + " && mkdir " + document.getElementById("folder_name").value);
+        document.getElementById("folder_name").value = "";
+    });
+    document.getElementById('gitInit').addEventListener('click', () => {
+        // initailzes git in current directory
+        execute_command("cd " + repoPath  + " && git init");
   });
 });
+
